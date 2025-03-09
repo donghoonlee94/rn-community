3a77080ae0ad02e1060971073064be7ccb4828b8
donghoonlee <94donghoon.lee@gmail.com>
2025-03-09 21:16:34 +0900

    feat: flexbox

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -8,25 +8,31 @@ import {
   View,
 } from "react-native";
 export default function HomeScreen() {
-  const [text, setText] = useState("");
   return (
     <SafeAreaView>
-      <View style={styles.container}>
-        <Text style={styles.text}>텍스트</Text>
+      <View style={styles.parent}>
+        <View style={styles.container}>
+          <Text style={styles.text}>텍스트</Text>
+        </View>
+        <View style={styles.container2}>
+          <Text style={styles.text}>텍스트</Text>
+        </View>
       </View>
-      <TextInput
-        value={text}
-        onChangeText={(text) => setText(text)}
-        style={styles.input}
-      />
-      <Button title="버튼" onPress={() => console.log(text)} />
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
+  parent: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
   container: {
     backgroundColor: "yellow",
+  },
+  container2: {
+    backgroundColor: "blue",
   },
   text: {
     color: "red",
