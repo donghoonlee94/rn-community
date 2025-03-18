881a53917bec21cd023dfb0ae5548cbd80fbabd0
donghoonlee <94donghoon.lee@gmail.com>
2025-03-18 16:34:24 +0900

    feat: feed list 마크업

--- components/FeedItem.tsx
+++ components/FeedItem.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import { Pressable, StyleSheet, Text, View } from "react-native";
 import { Octicons, MaterialCommunityIcons, Ionicons } from "@expo/vector-icons";
 import { Post } from "@/types";
+import Profile from "./Profile";
 
 interface FeedItemProps {
   post: Post;
@@ -14,6 +15,12 @@ function FeedItem({ post }: FeedItemProps) {
   return (
     <View style={styles.container}>
       <View style={styles.contentContainer}>
+        <Profile
+          onPress={() => {}}
+          nickname={post.author.nickname}
+          createdAt={post.createdAt}
+          imageUri={post.author.imageUri}
+        />
         <Text style={styles.title}>{post.title}</Text>
         <Text numberOfLines={3} style={styles.description}>
           {post.description}
