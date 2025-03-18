34d4c08f6c6bb8e5cb9d3360808fd81774ad1568
donghoonlee <94donghoon.lee@gmail.com>
2025-03-18 10:15:50 +0900

    feat: add CustomButton.tsx

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,8 +1,10 @@
+import CustomButton from "@/components/CustomButton";
 import { SafeAreaView, Text } from "react-native";
 export default function HomeScreen() {
   return (
     <SafeAreaView>
-      <Text>홈스크린</Text>
+      <Text>홈스큰</Text>
+      <CustomButton label="버튼" onPress={() => {}} />
     </SafeAreaView>
   );
 }
