7534c245d3ce1665c8d9cccac962ec00be171a5f
donghoonlee <94donghoon.lee@gmail.com>
2025-03-18 09:58:31 +0900

    feat: add vector-icons

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,6 +1,7 @@
 import { colors } from "@/constants";
 import { Tabs } from "expo-router";
 import React from "react";
+import Ionicons from "@expo/vector-icons/Ionicons";
 
 export default function TabLayout() {
   return (
@@ -14,18 +15,39 @@ export default function TabLayout() {
         name="index"
         options={{
           title: "Home",
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons
+              name={focused ? "home-sharp" : "home-outline"}
+              color={color}
+              size={25}
+            />
+          ),
         }}
       />
       <Tabs.Screen
         name="my"
         options={{
           title: "내 프로필",
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons
+              name={focused ? "person-circle-sharp" : "person-circle-outline"}
+              color={color}
+              size={25}
+            />
+          ),
         }}
       />
       <Tabs.Screen
         name="setting"
         options={{
           title: "설정",
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons
+              name={focused ? "settings" : "settings-outline"}
+              color={color}
+              size={25}
+            />
+          ),
         }}
       />
     </Tabs>
